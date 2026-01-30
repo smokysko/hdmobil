59861b7161a95fcddaf1d357a0e92b5ac521ef31
smokysko <utrustmeu@gmail.com>
2026-01-30 22:33:52 +0100

    Updated main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,8 +12,11 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
+const root = document.getElementById('root');
+if (root) {
+  createRoot(root).render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+}
